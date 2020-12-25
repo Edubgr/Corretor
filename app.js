9c994982cf8785b5b7ddd308c4ef1fd48fba0a9e
Eduardo <eduguibar@gmail.com>
2020-12-25 00:05:10 -0300

    asdfasd

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var compression = require('compression');
-var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var makepdfRouter = require('./routes/makepdf');
 
 var app = express();
-app.use(helmet());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -21,8 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(compression());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
